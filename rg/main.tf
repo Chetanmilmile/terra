a701286b455fdf105fcdd19371bcfb01e46d09e9
chetan milmile <107764112+Chetanmilmile@users.noreply.github.com>
2023-10-18 12:08:21 +0530

    push

--- rg/main.tf
+++ rg/main.tf
@@ -16,10 +16,30 @@ provider "azurerm" {
 variable "resource_group_names" {
   default = ["my-resource-group-1", "my-resource-group-2"]
 }
+variable "azurerm_virtual_network_hub" {
+  default = ["hub-v-net-1"]
+}
+variable "azurerm_virtual_network_spoke" {
+  default = ["spoke-v-net-1","spoke-v-net-1"]
+}
 
 # Create two resource groups
 resource "azurerm_resource_group" "example" {
   count = length(var.resource_group_names)
   name     = var.resource_group_names[count.index]
   location = "East US"  # Replace with your desired Azure region
+}
+
+resource "azurerm_virtual_network_hub" "example" {
+  name = var.azurerm_virtual_network_hub[count.index]
+  location = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space = ["10.0.0.0/24"]
+}
+
+resource "azurerm_virtual_network_spoke" "example" {
+  name = var.azurerm_virtual_network_spoke[count.index]
+  location = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space = ["10.0.0.0/24"]
 }
